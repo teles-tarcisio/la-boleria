6d647fd0eb03f0007179d926a92213766482ed4a
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2022-03-18 17:07:50 -0300

    feat: 'post /cakes' (at last)

--- src/controllers/cakesController.js
+++ src/controllers/cakesController.js
@@ -7,23 +7,20 @@ export async function insertCake(req, res) {
     const { rows: foundCakes } = await dbConnection.query(
       `
       SELECT * FROM cakes
-      WHERE unaccent(name) ILIKE $1`,
+      WHERE name ILIKE $1`,
       [name],
     );
-    console.log('!!', foundCakes);
     if (foundCakes.length > 0) {
       return res.status(409).send('Já existe bolo cadastrado com este nome');
     }
 
-    /*
     await dbConnection.query(`
-    INSERT INTO cakes (name, price, description, image)
-    VALUES ( $1, $2, $3, $4);
-  `, [name, price, description, image]);
-  */
+      INSERT INTO cakes (name, price, description, image)
+      VALUES ( $1, $2, $3, $4);
+      `, [name, price, description, image]);
+
+    return res.sendStatus(201);
   } catch (error) {
-    console.error(error);
     return res.status(500).send('!erro! cadastrando novo bolo');
   }
-  console.log('reached end');
 }
